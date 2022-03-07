cedd874ec70b560c98b7e724a69fece9257dc385
Mojtaba Abbasszadeh <mojtaba.abbasszadeh@gmail.com>
2022-03-06 20:46:42 -0500

    Init spring project

---
+++ src/main/java/com/example/chelteekeh/ChelTeekehApplication.java
@@ -0,0 +1,13 @@
+package com.example.chelteekeh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChelTeekehApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChelTeekehApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chelteekeh/ChelTeekehApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chelteekeh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChelTeekehApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
